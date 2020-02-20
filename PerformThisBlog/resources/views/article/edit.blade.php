cb6058c2e02a403159ea0cf8639fdea87ea231ad
unknown <bastiyanrodrigo@yahoo.co.uk>
2020-02-20 06:19:16 +0000

    Fixing Interface

--- PerformThisBlog/resources/views/article/edit.blade.php
+++ PerformThisBlog/resources/views/article/edit.blade.php
@@ -6,27 +6,23 @@
          <main class="pt-5 mx-lg-5">
   <!--Main layout-->
       {!! Form::model($article, ['method' => 'PUT', 'route' => ['article.update', $article->id], 'files' => true,]) !!}
-
-    <div class="card mb-4 wow fadeIn">
-    <!--Card content-->
-    <div class="card-body d-sm-flex justify-content-between">
-      <div class="md-form mt-3 w-100">
-        {!! Form::label('title', trans('Title').'', ['class' => 'control-label']) !!}
-        {!! Form::text('title', old('title'), ['class' => 'form-control', 'placeholder' => '','required']) !!}
-        @if($errors->has('title'))
-            <p class="help-block">
-                {{ $errors->first('title') }}
-            </p>
-        @endif
+      <div class="row">
+      <div class="col-12">
+     
+      <div class="px-2 md-form mt-3 w-100">
+      {!! Form::label('title', trans('Title').'', ['class' => 'control-label']) !!}
+      {!! Form::text('title', old('title'), ['class' => 'form-control', 'placeholder' => '','required']) !!}
+      @if($errors->has('title'))
+      <p class="help-block">
+      {{ $errors->first('title') }}
+      </p>
+      @endif
+    
       </div>
-    </div>
-</div>
-
-          <!--Grid row-->
-          <div class="row wow fadeIn">
-
-                <!--Grid column-->
-                <div class="col-md-12 mb-4">
+      </div>
+      </div>
+          <div class="row">
+                <div class="col-xl-8 mb-4">
         
                   <!--Card-->
                   <div class="card">
@@ -42,131 +38,111 @@
                         </p>
                     @endif
                 </div>
-            </div>
-        </div>
-    </div>
-         <!--Grid row-->
-         <div class="row wow fadeIn">
-                <!--Grid column-->
-                <div class="col-md-6 mb-4">
+                </div>
+               {!! Form::submit(trans('Update'), ['class' => 'btn btn-warning']) !!}
+
+                </div>
+                <div class="col-xl-4">
                   <!--Card-->
-                  <div class="card">
+                  <div class="card mb-4">
                     <!--Card content-->
                     <div class="card-body">
-                        	<div class="md-form mb-4 pink-textarea active-pink-textarea">
-                                    {!! Form::label('meta_description', trans('Summary').'', ['class' => 'control-label']) !!}
-                                    {!! Form::textarea ('meta_description', old('meta_description'), ['class' => 'md-textarea form-control', 'placeholder' => 'Please describe your article in 200 characters','maxlength' => '200','minlength' => '150','id'=>'summarybox','rows' => '3']) !!}
-                                     @if($errors->has('meta_description'))
-                                        <p class="help-block">
-                                            {{ $errors->first('meta_description') }}
-                                        </p>
-                                    @endif
-                            </div>
-                            <div id="textarea_feedback"></div>
-                        </div>
-
-                    </div>
-                    <!--/.Card-->
-          
-                  </div>
-
-                  <div class="col-md-3 mb-4">
-
-                        <!--Card-->
-                            <div class="card mb-4 card-cascade narrower">
-              
-                          <!-- Card header -->
-                         <div class="view view-cascade gradient-card-header blue-gradient">
-                              <h4 class="mb-0 font-weight-500">Tags</h4>
-                            </div>
-              
-                          <!--Card content-->
-                          <div class="card-body">
-                              
-                                {!! Form::select('tags[]', $tags, old('tags'), ['class' => 'js-example-basic-multiple w-100', 'multiple' => 'multiple','required']) !!}
-                                <p class="help-block"></p>
-                                @if($errors->has('tags'))
+                      <div class="md-form mb-4 pink-textarea active-pink-textarea">
+                                {!! Form::label('meta_description', trans('Summary').'', ['class' => 'control-label']) !!}
+                                {!! Form::textarea ('meta_description', old('meta_description'), ['class' => 'md-textarea form-control', 'placeholder' => 'Please describe your article in 200 characters','maxlength' => '200','minlength' => '150','id'=>'summarybox','rows' => '3']) !!}
+                                  @if($errors->has('meta_description'))
                                     <p class="help-block">
-                                        {{ $errors->first('tags') }}
+                                        {{ $errors->first('meta_description') }}
                                     </p>
                                 @endif
-                            </div>
-
                         </div>
-                        <!--/.Card-->
-              
-                      </div>
-                      <!--Grid column-->
-              
-                              <!--Grid column-->
-                      <div class="col-md-3 mb-4">
-              
+                        <div id="textarea_feedback"></div>
+                        </div>
+
+                   </div>
+                    <!--/.Card-->
+
                         <!--Card-->
-                            <div class="card mb-4">
+                      <div class="card mb-4">
               
                           <!-- Card header -->
                           <div class="card-header text-center">
-                            Cover Image
+                               Cover Image
                           </div>
-              
+
                           <!--Card content-->
                           <div class="card-body">
-                             <img class="card-img-top" src="{{ env('AWS_URL') .'thumbnails/'.$article->page_image }}" alt="Card image cap" style="max-width: 50%"> 
-                             
-                                <div class="file-field md-form">
-                                        <div class="btn btn-primary btn-sm float-left">
-                                                <span>Choose file</span>
-                                                <input type="file" name="page_image">
-                                        </div>
-                                        <div class="file-path-wrapper">
-                                             <input class="file-path validate" type="text" placeholder="Upload your file">
-                                        </div>
-                                 </div> 
-                                 @if($errors->has('page_image'))
-                                        <p class="help-block">
-                                            {{ $errors->first('page_image') }}
-                                        </p>
-                                    @endif	
-                                    </div>
-                                </div>
-                              <!--/.Card-->
-                    
-                            </div>
-                            <!--Grid column-->
-                                  <div class="col-md-3 mb-4">
-                    
-                              <!--Card-->
-                                  <div class="card mb-4">
-                    
-                                <!-- Card header -->
-                                <div class="card-header text-center">
-                                  Status
-                                </div>
-                    
-                                <!--Card content-->
-                                <div class="card-body">
-                                 {!! Form::select('status', $status, old('status'), ['class' => 'mdb-select md-form"','required']) !!}
-                                        @if($errors->has('status'))
-                                            <p class="help-block">
-                                                {{ $errors->first('status') }}
-                                            </p>
-                                        @endif
+                            <img class="card-img-top" src="{{ env('AWS_URL') .'thumbnails/'.$article->page_image }}" alt="Card image cap" style="max-width: 50%"> 
 
-                                    </div>
-                                </div>
-                              <!--/.Card-->
-                    
-                            </div>
-                    
-                    
+                          <div class="file-field md-form">
+                              <div class="btn btn-primary btn-sm float-left">
+                              <span>Choose file</span>
+                             <input type="file" name="page_image">
                           </div>
+                          <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text" placeholder="Upload your file">
+                          </div>
+                          </div> 
+                          @if($errors->has('page_image'))
+                          <p class="help-block">
+                          {{ $errors->first('page_image') }}
+                          </p>
+                          @endif	
+                          </div>
+                        </div>
+                              <!--/.Card-->
+                    <!--Card-->
+                    <div class="card mt-4 mb-4 card-cascade narrower">
+              
+                      <!-- Card header -->
+                      <div class="view view-cascade gradient-card-header blue-gradient">
+                      <h4 class="mb-0 font-weight-500">Tags</h4>
+                      </div>
+          
+                      <!--Card content-->
+                      <div class="card-body">
+                          
+                            {!! Form::select('tags[]', $tags, old('tags'), ['class' => 'js-example-basic-multiple w-100', 'multiple' => 'multiple','required']) !!}
+                            <p class="help-block"></p>
+                            @if($errors->has('tags'))
+                                <p class="help-block">
+                                    {{ $errors->first('tags') }}
+                                </p>
+                            @endif
+                        </div>
+  
+                      </div>
+                    <!--/.Card-->
+                    
+                          <!--Card-->
+                          <div class="card mb-4">
+
+                            <!-- Card header -->
+                            <div class="card-header text-center">
+                            Status
+                            </div>
+
+                            <!--Card content-->
+                            <div class="card-body">
+                            {!! Form::select('status', $status, old('status'), ['class' => 'mdb-select md-form"','required']) !!}
+                            @if($errors->has('status'))
+                            <p class="help-block">
+                            {{ $errors->first('status') }}
+                            </p>
+                            @endif
+
+                            </div>
+                            </div>
+                            <!--/.Card-->
+                            </div>
+                      </div>
                           <!--Grid row-->
                           <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
                
                         </div>
                 
       
-      {!! Form::submit(trans('Update'), ['class' => 'btn btn-warning']) !!}
+
       {!! Form::close() !!}
     </main>
       @endsection
--- PerformThisBlog/resources/views/article/index.blade.php
+++ PerformThisBlog/resources/views/article/index.blade.php
@@ -17,32 +17,47 @@
 <div class="card card-cascade">
 
         <!-- Card image -->
+        <div class="row">
+          <div class="col-md-4 pr-0">
+            @if($article->page_image)   
+          
+              <img class="card-img-top img-thumbnail" src="{{ env('AWS_URL') .'thumbnails/'.$article->page_image }}" alt="Card image cap">
+    
+            @endif
+
+            <div class="btn-group" role="group" aria-label="Basic example">
+            <!-- View -->
+            <a class="btn btn-floating btn-small btn-tw" href="{{ env('BLOG_VIEW_URI') .'/'. $article->slug }}" target="_blank"><i class="fas fa-eye"></i></a>
+            <!-- Edit -->
+            <a type="button" class="btn btn-floating btn-small btn-dribbble" href="{{ route('article.edit', $article->id )}}"><i class="fas fa-pencil-alt"></i></a>
+            </div> 
+
+          </div>
+          <div class="col-md-8 pl-0">
+            <div class="card-body card-body-cascade">
+      
+              <!-- Title -->
+              <a href="{{ route('article.edit', $article->id )}}"><h4 class="card-title"><strong>{{$article->title}}</strong></h4></a>
+              <!-- Subtitle -->
+              <!-- Text -->
+              <p class="card-text">
+                  {{ strip_tags(str_limit($article->content, $limit=150, $end = '....')) }}
+              </p>
+          
+ 
+            </div>
+          </div>
+        </div>
+
         <div class="view view-cascade overlay">
-          @if($article->page_image)   
-          <img class="card-img-top img-thumbnail" src="{{ env('AWS_URL') .'thumbnails/'.$article->page_image }}" alt="Card image cap" style="max-height: 160px;">
-          @endif
+         
           <a>
             <div class="mask rgba-white-slight"></div>
           </a>
         </div>
       
         <!-- Card content -->
-        <div class="card-body card-body-cascade text-center">
       
-          <!-- Title -->
-          <h4 class="card-title"><strong>{{$article->title}}</strong></h4>
-          <!-- Subtitle -->
-          <!-- Text -->
-          <p class="card-text">
-              {{ strip_tags(str_limit($article->content, $limit=150, $end = '....')) }}
-          </p>
-      
-          <!-- Twitter -->
-          <a type="button" class="btn-floating btn-small btn-tw" href="{{ env('BLOG_VIEW_URI') .'/'. $article->slug }}"><i class="fas fa-eye"></i></a>
-          <!-- Google + -->
-          <a type="button" class="btn-floating btn-small btn-dribbble" href="{{ route('article.edit', $article->id )}}"><i class="fas fa-pencil-alt"></i></a>
-      
-        </div>
       
       </div>
       <!-- Card Regular -->
